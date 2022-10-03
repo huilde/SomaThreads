955f8cf6dc22cebced7408786f7b4eab0ee19bb3
huilde <huilde@DESKTOP-4O64BCP.localdomain>
2022-10-02 23:22:28 -0300

    threads

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,89 @@
-#include <chrono>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
+// C Program to multiply two matrix using pthreads without
+// use of global variables
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<iostream>
 
-#include "matrix.hpp"
+#define MAX 3
 
-using std::cout;
-using std::ifstream;
-using std::ofstream;
-using std::pair;
-using std::string;
-using std::stringstream;
-using std::vector;
-using std::chrono::duration_cast;
-using std::chrono::nanoseconds;
-using std::chrono::steady_clock;
+int NUM_THREADS = 3;
+int NUM_P = 4;
 
-bool CanMultiply(const vector<vector<int>> &fst_matrix,
-                 const vector<vector<int>> &snd_matrix) {
-  return false;
+
+int matA[MAX][MAX];
+int matB[MAX][MAX];
+int matC[MAX][MAX];
+
+int linha = 0;
+int coluna = 0;
+int algo = 0;
+void* tarefa(void *tid)
+{
+    int QuantosCalcular = 0; 
+      printf("\nThread\n");
+      printf("%d",((int)(size_t) tid));
+      for( linha ; linha < MAX; ++linha){
+        if(QuantosCalcular==NUM_P){
+            break;
+        }
+        for(algo  ; algo < MAX; ++algo){
+             if(QuantosCalcular==NUM_P){
+            break;
+        }
+            for(coluna = 0 ; coluna < MAX; ++coluna)
+            {
+                matC[linha][algo] += matA[linha][coluna] * matB[coluna][algo];
+            }
+        printf("%d", algo);
+        printf("\n");
+        QuantosCalcular++;
+}
+}
 }
 
-void Multiply(const vector<vector<int>> &fst_matrix,
-              const vector<vector<int>> &snd_matrix) {
-  vector<vector<int>> result;
 
-}
 
-pair<int, int> GetMatrixDimensions(const string filename) {
-  pair<int, int> dimensions(0, 0);
-  ifstream file(filename);
-  string line;
+int main()
+{
+    int r1=MAX,c1=MAX,r2=MAX,c2=MAX,i,j,k;
 
-  if (!getline(file, line)) return pair<int, int>(0, 0);
-  dimensions.first++;
+    // Generating random values in matA
+    for (i = 0; i < r1; i++)
+            for (j = 0; j < c1; j++)
+                   matA[i][j] = rand() % 10;
+           
+        // Generating random values in matB
+    for (i = 0; i < r1; i++)
+            for (j = 0; j < c1; j++)
+                   matB[i][j] = rand() % 10;
+    
+    pthread_t threads[NUM_THREADS];
+   printf("Inicio das Threads...\n");
+   
+    // inicia as  threads, passando como parametroa thread e a função que cada uma deve executar
+   for (size_t i = 0; i < NUM_THREADS; i++)
+   {
+    pthread_create(&threads[i], NULL, tarefa, NULL);
 
-  stringstream ss(line);
-  string cell;
+   }
 
-  while (ss >> cell) dimensions.second++;
-  while (getline(file, line)) dimensions.first++;
+   
+    // faz com que a thread principal espere as threads acabarem;
 
-  return dimensions;
-}
+ for (size_t i = 0; i < NUM_THREADS; i++)
+   {
+    pthread_join(threads[i], NULL);
+   }
+    
+    printf("resultado da tabela 3\n");
 
-int main(int argc, char **argv) {
-  auto fst_dimensions = GetMatrixDimensions(argv[1]);
-  auto snd_dimensions = GetMatrixDimensions(argv[2]);
-
-  Matrix m1 = Matrix(fst_dimensions.first, fst_dimensions.second);
-  Matrix m2 = Matrix(snd_dimensions.first, snd_dimensions.second);
-
-  ifstream file1(argv[1]);
-  ifstream file2(argv[2]);
-  ofstream file3("matrix3.txt");
-
-  file1 >> m1;
-  file2 >> m2;
-
-  auto start = steady_clock::now();
-  auto m3 = m1 * m2;
-  auto end = steady_clock::now();
-  auto duration = duration_cast<nanoseconds>(end - start).count();
-
-  file3 << m3 << "\n";
-  file3 << duration << " ns\n";
+    
+        for(int i = 0;i<MAX; i++){
+                for(int j= 0; j<MAX;j++){
+              printf("%i\t  ",matC[i][j]);
+                }
+                printf("\n");
+        }
+    exit(0);
 }
